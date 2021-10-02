3bd4b276ebb263acbf0f4870787999327ed3a475
Tracyfoster <tracyfoster99@gmail.com>
2021-10-02 05:29:53 +0100

    Course tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,20 @@
 FactoryBot.define do
+  factory :course do
+    title { "Crytocurrency, The Hard Parts" }
+    description { Faker::Lorem.paragraph }
+    expertise { "expert" }
+    industry { "Finance" }
+
+    factory :invalid_course do
+      title { nil }
+    end
+  end
+
   factory :user do
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
     email { Faker::Internet.unique.email }
-    expertise { "advanced" }
+    expertise { "expert" }
 
     factory :invalid_user do
       email { nil }
