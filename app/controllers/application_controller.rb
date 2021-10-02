1b9abce6b867c053e2761cb7636f3ee6040a2ff5
Tracyfoster <tracyfoster99@gmail.com>
2021-10-02 10:12:12 +0100

    Rescue not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found(exception)
+    render json: { message: "Requested #{exception.model} Record does not exist." },
+           status: :not_found
+  end
 end
